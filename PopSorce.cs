3cdafba4002db914aec4a10a624b54323a78ea35
mitsuru1397 <g1527068@st.t-kougei.ac.jp>
2018-12-17 17:52:10 +0900

    Add files via upload

---
+++ PopSorce.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopScore : MonoBehaviour {
+
+    public float popNetxTime;
+    public GameObject enemy;
+
+    private float elapsedTime;
+
+    float enemyPopPosX;
+    float enemyPopPosY;
+
+	// Use this for initialization
+	void Start () 
+    {
+
+        elapsedTime = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+        elapsedTime += Time.deltaTime;
+
+        if(elapsedTime > popNetxTime)
+        {
+            elapsedTime = 0f;
+            
+            PopEnemy ();
+        }
+	}
+
+    void PopEnemy()
+    {
+         //エネミー出現位置決定
+	    enemyPopPosX = Random.Range(20.0f, 40.0f);
+        enemyPopPosY = Random.Range(-2.0f, 2.0f);
+
+
+        //出現処理
+        Instantiate(enemy, new Vector2(enemyPopPosX, enemyPopPosY), Quaternion.identity);
+
+    }
+}
---
+++ popScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class popScript : MonoBehaviour {
+
+    public float popNextTime;
+    public GameObject enemy;
+
+    private float elapsedTime;
+
+    float enemyPopPosX;
+    float enemyPopPosY;
+    float popPosRam;
+
+	// Use this for initialization
+	void Start () {
+        elapsedTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        elapsedTime += Time.deltaTime;
+
+        if(elapsedTime > popNextTime)
+        {
+            elapsedTime = 0f;
+
+            PopEnemy();
+        }
+		
+	}
+
+    void PopEnemy()
+    {
+        //エネミー出現位置決定
+        //enemyPopPosX = Random.Range(10.0f, 11.0f);
+        popPosRam = Random.Range(0f, 1f);
+        if (popPosRam > 0.5)
+            enemyPopPosX = Random.Range(10.0f, 11.0f);
+        else
+            enemyPopPosX = Random.Range(-10.0f, -11.0f);
+
+        enemyPopPosY = Random.Range(-2.0f, 4.0f);
+
+        Instantiate(enemy, new Vector2(enemyPopPosX, enemyPopPosY), Quaternion.identity);
+    }
+}
