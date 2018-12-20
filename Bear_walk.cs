3cc52672117a0a34e66ba25b13c3740b2bb5215d
mitsuru1397 <g1527068@st.t-kougei.ac.jp>
2018-12-20 19:08:52 +0900

    Update Bear_walk.cs

--- Bear_walk.cs
+++ Bear_walk.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,6 +8,7 @@ public class Bear_walk : MonoBehaviour {
     Rigidbody2D rb2d;
     private bool isJump = false;
     private bool isGround = true;
+    private bool mouseControl;
     private int jumpCount = 0;
     private int MAX_JUMP_COUNT = 2;
     private float moveeffect = 1.0f;
@@ -16,18 +17,23 @@ public class Bear_walk : MonoBehaviour {
     string state;
     string prevState;
 
+    public Vector3 playerTransform;
+
+    public GameObject slash;
+
     public Sprite[] animeSte = new Sprite[3];
     int jumpCheck = 0;
 
     // Use this for initialization
     void Start ()
     {
-
+        mouseControl = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         this.rb2d = GetComponent<Rigidbody2D>();
+        Attack();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -44,6 +50,7 @@ public class Bear_walk : MonoBehaviour {
     void FixedUpdate()
     {
         Move();
+        //Attack();
     }
 
     void Move()
@@ -77,6 +84,8 @@ public class Bear_walk : MonoBehaviour {
             Physics.gravity = new Vector2(0f, 0f);
             transform.Translate(new Vector3(-0.15f * moveeffect * Dush, 0.0f));
             transform.localScale = new Vector2(-1, 1);
+
+            playerTransform = new Vector3(-0.15f * moveeffect * Dush, 0.0f);
         }
 
         else if (Input.GetKey(KeyCode.D))
@@ -84,6 +93,12 @@ public class Bear_walk : MonoBehaviour {
             Physics.gravity = new Vector2(0f, 0f);
             transform.Translate(new Vector3(0.15f * moveeffect * Dush, 0.0f));
             transform.localScale = new Vector2(1, 1);
+
+            playerTransform = new Vector3(0.15f * moveeffect * Dush, 0.0f);
+        }
+        else
+        {
+            playerTransform = new Vector3(0.00f * moveeffect * Dush, 0.0f);
         }
 
         if (jumpCount < MAX_JUMP_COUNT && Input.GetKeyDown(KeyCode.Space))
@@ -95,5 +110,19 @@ public class Bear_walk : MonoBehaviour {
         }
 
         Dush = 1.0f;
+    }
+
+    void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && mouseControl == false)
+        {
+            Debug.Log("Mouse0");
+            Instantiate(slash, transform.position, transform.rotation);
+            mouseControl = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            mouseControl = false;
+        }
     }
 }
